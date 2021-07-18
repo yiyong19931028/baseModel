6d3a7b17ec94f68f9c6bf3e9efd89dac1b9cde40
yiyong19931028 <15091869051@163.com>
2021-07-18 23:49:23 +0800

    基础架构

---
+++ model-comm/src/main/java/com/base/modelcomm/modelcomm/ModelCommApplication.java
@@ -0,0 +1,13 @@
+package com.base.modelcomm.modelcomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelCommApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModelCommApplication.class, args);
+    }
+
+}
---
+++ model-comm/src/test/java/com/base/modelcomm/modelcomm/ModelCommApplicationTests.java
@@ -0,0 +1,13 @@
+package com.base.modelcomm.modelcomm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelCommApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
